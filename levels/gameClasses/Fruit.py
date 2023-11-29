e24921593fdddba8753df46b3cf91021597e23b3
Ivan Sukhankin <sukhankin.is@phystech.edu>
2023-11-29 21:59:45 +0300

    fruit_t color team and collision

--- levels/gameClasses/Fruit.py
+++ levels/gameClasses/Fruit.py
@@ -17,12 +17,19 @@ class FRUIT:
 
 class FRUIT_t:
     # Временая версия фрукта (не появляется еще раз)
-    def __init__(self, pos):
+    def __init__(self, pos, team = None):
         self.pos = pos
+        self.team = team
+        if team == 'blue':
+            self.color = blue_snake_fruit_t_color
+        elif team == 'red':
+            self.color = red_snake_fruit_t_color
+        else:
+            self.color = fruit_t_color
 
     def draw_fruit_t(self):
         fruit_rect = pygame.Rect(int(self.pos.x * cell_size), int(self.pos.y * cell_size), cell_size, cell_size)
-        pygame.draw.rect(screen, fruit_t_color, fruit_rect)
+        pygame.draw.rect(screen, self.color, fruit_rect)
 
 
 class FRUIT_cut:
